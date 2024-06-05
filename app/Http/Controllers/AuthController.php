7d0ada4562141bff50ab54bee21de024c2393980
rakaism <rakadianp@gmail.com>
2024-06-05 10:04:33 +0700

    rename route auth to login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -46,10 +46,10 @@ class AuthController extends Controller
                 return redirect()->route('seller')->with('success', 'Anda berhasil Login');
             } else {
                 Auth::logout();
-                return redirect()->route('auth')->withErrors('Akun Belum Aktif');
+                return redirect()->route('login')->withErrors('Akun Belum Aktif');
             }
         } else {
-            return redirect()->route('auth')->withErrors('Email atau Sandi salah');
+            return redirect()->route('login')->withErrors('Email atau Sandi salah');
         }
     }
     function create()
