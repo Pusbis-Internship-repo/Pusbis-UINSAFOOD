b68a22eec8aec74718088e69f4eb97f1a837b877
iranel99 <irwan1236@gmail.com>
2024-12-23 11:48:46 +0700

    updateImage

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -69,21 +69,25 @@ class MenuController extends Controller
         //Jika terdapat file gambar yang diupload
         if ($request->hasFile('menu_pic')) {
             $image = $request->file('menu_pic');
+
             // Ubah nama file gambar menjadi ID makanan
             $imageName = $menuId . '.' . $image->getClientOriginalExtension();
             // dd($imageName, $menu);
 
             //Resize ukuran gambar menu
             $resizedImage = Image::make($image)->fit(600, 520)->encode();
+
             //Tentukan path penyimpanan baru
-            // $imagePath = 'public/menu_images/' . $imageName;
-            $path = $request->file('menu_pic')->store('menu_images', 'public');
+            $imagePath = 'public/menu_images/' . $imageName;
+
+            // $path = $request->file('menu_pic')->store('menu_images/asdas.png', 'public');
             // dd($path);
+
             //Simpan gambar yang telah diresize ke dalam penyimpanan
-            // Storage::put($imagePath, $resizedImage);
+            Storage::put($imagePath, $resizedImage);
 
             // Update path gambar pada model Menu
-            $menu->menu_pic = $path;
+            $menu->menu_pic = $imagePath;
 
             //Menyimpan menu ke database
             $menu->save();
